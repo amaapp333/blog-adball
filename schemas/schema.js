65bbbdeb0fe13cf635a74d7b8dca4ddcd739125f
amaapp333 <118077998+amaapp333@users.noreply.github.com>
2022-11-28 19:41:50 +0200

    Create schema.js

---
+++ schemas/schema.js
@@ -0,0 +1,1 @@
+import createSchema from "part:@sanity/base/schema-creator"; import schemaTypes from "all:part:@sanity/base/schema-type"; import maliek from './maliek' // import the file you just made export default createSchema({ name: "default", types: schemaTypes.concat([ 		animal, // add the document type to this array ]), });
